b70fa66a49af00065a6331a0bbdc0fb56c6b07ff
Laura <laura@pc-53.home>
2021-01-19 16:38:24 +0100

    AJout menu choix utilisateur

--- algojour2-YE/block.py
+++ algojour2-YE/block.py
@@ -5,13 +5,15 @@ from datetime import datetime
 
 
 class Block:
-    def __init__(self, previous_hash, data, signature, preuve_travail):
+    def __init__(self, previous_hash, data, signature, preuve_travail, index):
         self.previous_hash = previous_hash
         self.data = data
         self.signature = signature
         self.preuve_travail = preuve_travail
         self.creation_date = datetime.now()
         self.hash = ""
+        self.index = index
+        self.nounce = 0
 
     @staticmethod
     def is_format_hash_ok(hash_block, nbr_zero):
@@ -20,30 +22,34 @@ class Block:
             zero_chaine += "0"
         return hash_block.startswith(zero_chaine)
 
-    @staticmethod
-    def generate_preuvre_travail(preuve_travail):
+    def generate_preuvre_travail(self):
         letters = string.ascii_letters
-        result_str = ''.join(random.choice(letters) for i in range(2))
-        return preuve_travail + result_str
+        result_str = ''.join(random.choice(letters) for i in range(8))
+        return self.preuve_travail + result_str
 
     def create_hash(self, nbr_zero):
         new_preuve_travail = self.preuve_travail
         bloc = str(self.previous_hash) + str(self.data) + str(self.signature) + str(new_preuve_travail) \
             + str(self.creation_date)
         hash_block = sha256(bloc.encode('utf-8')).hexdigest()
+        nounce = 0
         while not self.is_format_hash_ok(hash_block, nbr_zero):
-            new_preuve_travail = self.generate_preuvre_travail(self.preuve_travail)
+            nounce += 1
+            new_preuve_travail = self.generate_preuvre_travail()
             bloc = str(self.previous_hash) + str(self.data) + str(self.signature) + str(new_preuve_travail) \
             + str(self.creation_date)
             hash_block = sha256(bloc.encode('utf-8')).hexdigest()
         self.hash = hash_block
+        self.nounce = nounce
         self.preuve_travail = new_preuve_travail
         return self
 
     def __str__(self):
-        return "\tPrevious Hash : " + self.previous_hash \
+        return "\tIndex : " + str(self.index) \
+            + "\n\tPrevious Hash : " + self.previous_hash \
             + "\n\tData : " + self.data \
             + "\n\tSignature : " + self.signature \
             + "\n\tPreuve de travail : " + self.preuve_travail \
             + "\n\tCreation Date : " + str(self.creation_date) \
-            + "\n\tHash : " + self.hash
+            + "\n\tHash : " + self.hash \
+            + "\n\tNounce : " + str(self.nounce)
--- algojour2-YE/blockchaine.py
+++ algojour2-YE/blockchaine.py
@@ -2,22 +2,44 @@ import block
 
 
 class Blockchaine:
-    def __init__(self):
+    def __init__(self, nbr_zero):
         self.blockchaines = []
+        self.nbr_zero = nbr_zero
 
-    def add_block(self, nbr_zero, data):
+    def add_block(self, data, signature):
         if not self.blockchaines:
-            new_block = block.Block("NONE", data, "Crée par", "preuveTravail")
+            new_block = block.Block("NONE", data, signature, "", 0)
         else:
             previous_hash = self.blockchaines[len(self.blockchaines) - 1].hash
-            new_block = block.Block(previous_hash, data, "Crée par", "preuveTravail")
-        new_block = new_block.create_hash(nbr_zero)
+            new_block = block.Block(previous_hash, data, signature, "", len(self.blockchaines))
+        new_block = new_block.create_hash(self.nbr_zero)
         self.blockchaines.append(new_block)
 
+    def is_format_all_hash_valid(self):
+        is_valid = True
+        for i in range(len(self.blockchaines)):
+            if i == 0:
+                is_valid = is_valid and self.blockchaines[i].is_format_hash_ok(self.blockchaines[i].hash, self.nbr_zero)
+            else :
+                is_valid = is_valid and self.blockchaines[i].is_format_hash_ok(self.blockchaines[i].previous_hash, self.nbr_zero)
+                is_valid = is_valid and self.blockchaines[i].is_format_hash_ok(self.blockchaines[i].hash, self.nbr_zero)
+        return is_valid
+
+    def is_all_hash_equal_previous_hash(self):
+        is_valid = True
+        for i in range(1, len(self.blockchaines)):
+            is_valid = is_valid and self.blockchaines[i-1].hash == self.blockchaines[i].previous_hash
+        return is_valid
+
+    def is_blockchaine_valid(self):
+        is_valid = self.is_format_all_hash_valid()
+        is_valid = is_valid and self.is_all_hash_equal_previous_hash()
+        return is_valid
+
     def __str__(self):
-        result = ""
         if not self.blockchaines:
             return "Aucun élément dans la liste."
+        result = "Blockchaine vérifiée : " + str(self.is_blockchaine_valid()) + "\n"
         for i in range(len(self.blockchaines)):
             result += "Block n° "
             result += str(i)
@@ -26,12 +48,40 @@ class Blockchaine:
             result += "\n]\n" 
         return result
 
+    def save(self):
+        print("Sauvegarde réussie ! ")
 
-blockchaine = Blockchaine()
-print("***** TESTE SANS AJOUT ********")
-print(blockchaine)
-print("***** TESTE AJOUT ********")
-blockchaine.add_block(2, "Genesis block")
-blockchaine.add_block(2, "First block")
-blockchaine.add_block(2, "Second block")
-print(blockchaine)
+    @staticmethod
+    def menu():
+        affichage = "******************* MENU *******************"
+        affichage += "\n Taper a pour afficher la blockchaine."
+        affichage += "\n Taper s pour sauvegarder."
+        affichage += "\n Taper x pour ajouter un block."
+        affichage += "\n Taper m pour afficher le menu."
+        affichage += "\n Taper q pour quitter."
+        affichage += "\n*****************************************"
+        return affichage
+
+    def choix(self):
+        choix = ""
+        while choix != "q":
+            choix = input("Taper votre choix : ")
+            if choix == "a":
+                print(blockchaine)
+            elif choix == "s":
+                save(blockchaine)
+            elif choix == "x":
+                data = input("Taper votre donnée : ")
+                signature = input("Taper votre signature : ")
+                blockchaine.add_block(data, "Crée par " + signature)
+            elif choix == "m":
+                print(blockchaine.menu())
+            elif choix == "q":
+                print("Au revoir !")
+            else :
+                print("Choix incorrect")
+                self.blockchaines.menu()
+
+blockchaine = Blockchaine(2)
+print(blockchaine.menu())
+blockchaine.choix()
